12e50b2297b42c88a87aa5d9972666e578febf74
Luis Ortiz <l.ortizintec@gmail.com>
2023-04-10 14:29:27 -0400

    got getjokes working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,26 @@ $( document ).ready( onReady );
 
 function onReady() {
     console.log('DOM ready');
+    $('#addJokeButton').on('click', addJoke);
+    getJokes();
+}
+
+
+
+function getJokes(){
+    $.ajax({
+        method: 'GET',
+        url: '/jokes'
+    }).then(function(jokes){
+        console.log(jokes);
+        $('#outputDiv').empty();
+        for(let joke of jokes){
+            $('#outputDiv').append(`
+            <div>
+            <h3>${joke.whoseJoke}</h3>
+            <p>${joke.jokeQuestion} ${joke.punchLine}</p>
+            </div>
+            `)
+        }
+    })
 }
